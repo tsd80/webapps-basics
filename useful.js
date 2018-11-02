322dfb31c025333e4d97b73f00d75aa096bdcc0b
Dmitri Tsyganok <dmitri@tsyganok.net>
2018-11-02 09:09:40 +0200

    Update

--- useful.js
+++ useful.js
@@ -2,3 +2,11 @@
 function getRandomInt(min, max) {
   return min + Math.floor(Math.random() * (max - min + 1));
 }
+
+//Fibonacci calculation in recursive function
+function fib(n) {
+  if (n <= 2){
+    return 1
+  }
+  return fib(n - 1) + fib (n - 2)
+}
