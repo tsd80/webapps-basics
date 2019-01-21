3ecb664848c84106e44e9579770706bb45c5c79d
Dmitri Tsyganok <dmitri@tsyganok.net>
2019-01-21 22:47:29 +0200

    Euclidean algorithm

--- useful.js
+++ useful.js
@@ -10,3 +10,11 @@ function fib(n) {
   }
   return fib(n - 1) + fib (n - 2)
 }
+
+//Euclidean algorithm
+const calcGCD = (a, b) =>{
+  //when a%b=0, returning b
+  if (a%b===0) {return b}
+  //recursively entering function with ex. 10%15=
+  return calcGCD(b, a%b);
+};
